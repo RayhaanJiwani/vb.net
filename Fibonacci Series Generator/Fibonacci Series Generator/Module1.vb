2a3ecd9979518e594ce889679856c1938ed07485
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:39:13 +0530

    Add files via upload

---
+++ Fibonacci Series Generator/Fibonacci Series Generator/Module1.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim len, t1, t2, t3 As Integer
+        Console.WriteLine("Please enter the number of terms in your sequence")
+        len = Console.ReadLine()
+        t1 = 1
+        t2 = 2
+        t3 = 0
+
+        If len = 1 Then
+            Console.WriteLine("1")
+        ElseIf len = 2 Then
+            Console.WriteLine("1 1")
+        ElseIf len = 3 Then
+            Console.WriteLine("1 1 2")
+        Else
+            Console.Write("1 1 2")
+            For counter = 4 To len
+                t3 = t1 + t2
+                Console.Write(" " & t3 & " ")
+                t1 = t1 + t2
+                t2 = t1 + t2
+                Console.Write(t2)
+            Next
+        End If
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Fibonacci Series Generator/Fibonacci Series Generator/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Fibonacci Series Generator")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Fibonacci Series Generator")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("eb0b99ec-7396-4766-b3c8-e09186b755d6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
