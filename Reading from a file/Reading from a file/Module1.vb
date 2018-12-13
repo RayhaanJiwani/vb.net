59ffdb761d49ae0188611fd1c99e04325b7bab57
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:37:54 +0530

    Add files via upload

---
+++ Reading from a file/Reading from a file/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FileHandle_W As IO.StreamWriter
+        Dim FileHandle_R As IO.StreamReader
+        FileHandle_W = New IO.StreamWriter("C:\Users\rayhaan.jiwani\Desktop\Sample.txt", True)
+        FileHandle_R = New IO.StreamReader("C:\Users\rayhaan.jiwani\Desktop\Sample2.txt")
+        Do Until FileHandle_R.EndOfStream
+            Str = 
+        Loop
+    End Sub
+
+End Module
