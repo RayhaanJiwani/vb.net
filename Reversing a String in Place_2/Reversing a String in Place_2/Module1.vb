4e4af5477f425a4bab4408d1b94405ee3c5e893e
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:39:59 +0530

    Add files via upload

---
+++ Reversing a String in Place_2/Reversing a String in Place_2/Module1.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s_length, counter, limit As Integer
+        Dim str As String
+        Dim Array_Str(30) As Char
+        counter = 1
+        str = Console.ReadLine()
+        s_length = Len(str)
+        If s_length Mod 2 = 0 Then
+            limit = (s_length) / 2
+        Else
+            limit = ((s_length) + 1) / 2
+        End If
+        For counter = 1 To s_length
+            Array_Str(counter) = Mid(str, counter, 1)
+        Next
+        If (((s_length) Mod (2)) = 0) Then
+            Reverse(limit, s_length, Array_Str)
+        ElseIf (((s_length) + 1) Mod (2)) = 0 Then
+            Reverse(limit, s_length, Array_Str)
+        End If
+
+    End Sub
+
+    Sub Reverse(ByVal l, ByVal a, ByVal Array_Str)
+        Dim i, j, counter As Integer
+        Dim temp As Char
+        i = 1
+        j = a
+        counter = 1
+        While (i < l) And (j > l)
+            temp = Array_Str(i)
+            Array_Str(i) = Array_Str(j)
+            Array_Str(j) = temp
+            i = i + 1
+            j = j - 1
+        End While
+        For counter = 1 To a
+            Console.Write(Array_Str(counter))
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
+
---
+++ Reversing a String in Place_2/Reversing a String in Place_2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Reversing a String in Place_2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Reversing a String in Place_2")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76febbe8-d2e5-4685-8b60-8255f04f5d74")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
