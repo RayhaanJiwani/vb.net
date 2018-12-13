dcc1c70f1bb75b7866b7c1b5dc2e5247a239981b
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:43:08 +0530

    Add files via upload

---
+++ Check whether a name is present in a file/Check whether a name is present in a file/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FileRead As IO.StreamReader
+        Dim str As String
+        Dim found As Boolean
+        found = False
+        FileRead = New IO.StreamReader("C:\Users\rayhaan.jiwani\Desktop\Names.txt")
+        Console.WriteLine("Please enter the name you are looking for")
+        str = Console.ReadLine()
+        Do Until FileRead.EndOfStream
+            If FileRead.ReadLine = str Then
+                found = True
+            End If
+        Loop
+        FileRead.Close()
+        If found = True Then
+            Console.WriteLine("The name is present in the file")
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
