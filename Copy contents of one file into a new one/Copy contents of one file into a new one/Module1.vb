5f288055c6232d3c10c38f0fac82267232c88247
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:41:09 +0530

    Add files via upload

---
+++ Copy contents of one file into a new one/Copy contents of one file into a new one/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FileRead As IO.StreamReader
+        Dim FileWrite As IO.StreamWriter
+        Dim str As String
+        FileRead = New IO.StreamReader("C:\Users\rayhaan.jiwani\Desktop\Text.txt")
+        FileWrite = New IO.StreamWriter("C:\Users\rayhaan.jiwani\Desktop\Text_Copy.txt")
+        While FileRead.EndOfStream = False
+            str = FileRead.ReadLine
+            FileWrite.WriteLine(str)
+        End While
+        FileRead.Close()
+        FileWrite.Close()
+        Console.WriteLine("The contents of the file have been copied")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Copy contents of one file into a new one/Copy contents of one file into a new one/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Copy contents of one file into a new one")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Copy contents of one file into a new one")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("aa704d55-5b73-4978-aba5-63e4a4ab2379")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
