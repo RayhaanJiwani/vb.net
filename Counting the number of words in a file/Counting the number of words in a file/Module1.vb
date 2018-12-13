9e826ded4b864b7863553f4b228be4fab5316562
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:44:35 +0530

    Add files via upload

---
+++ Counting the number of words in a file/Counting the number of words in a file/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FileRead As IO.StreamReader
+        Dim str As String
+        Dim str_len, words As Integer
+        words = 0
+        FileRead = New IO.StreamReader("C:\Users\rayhaan.jiwani\Desktop\Counting words.txt")
+        Do Until FileRead.EndOfStream
+            str = FileRead.ReadLine()
+            str_len = Len(str)
+            For counter = 1 To str_len
+                If Mid(str, counter, 1) = " " Then
+                    words = words + 1
+                End If
+            Next
+            words = words + 1
+        Loop
+        Console.WriteLine("The number of words in the file is " & (words))
+        Console.ReadLine()
+    End Sub
+
+End Module
