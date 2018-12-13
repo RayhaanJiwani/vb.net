d0a45d6e300c95b78586e5bb19a7053730b61dad
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:39:16 +0530

    Add files via upload

---
+++ Automorphic Number Checker/Automorphic Number Checker/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num, tempsq, counter As Integer
+        Dim templen, length, square As String
+        Dim automorphic_yes As Boolean
+        automorphic_yes = True
+        counter = 1
+        Console.WriteLine("Please enter the number to be checked")
+        num = Console.ReadLine()
+        templen = CStr(num)
+        tempsq = num ^ 2
+        square = CStr(tempsq)
+        length = Len(templen)
+
+        While (counter <= length) And (automorphic_yes = True)
+            If Right(templen, counter) = Right(square, counter) Then
+                automorphic_yes = True
+            Else
+                automorphic_yes = False
+            End If
+            counter = counter + 1
+        End While
+
+        If automorphic_yes = True Then
+            Console.WriteLine("The number input is automorphic")
+        Else
+            Console.WriteLine("The number input is not automorphic")
+        End If
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ Automorphic Number Checker/Automorphic Number Checker/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Automorphic Number Checker")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Automorphic Number Checker")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("59fe5e4b-32f0-4106-8be7-dbc0a8e742fc")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
