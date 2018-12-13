236154806f99fb5a03847a8f3f7fca456d934d69
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:45:26 +0530

    Add files via upload

---
+++ Times Table/Times Table/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim multiplier, iterations As Integer
+        Console.Writeline("Please enter the number whose times table you want")
+        multiplier = Console.Readline()
+        Console.Writeline("Please enter the number till which you want multiples")
+        iterations = Console.Readline()
+
+        For counter = 1 To iterations
+            Console.Writeline(multiplier & " * " & counter & " = " & (multiplier * counter))
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+
+End Module
