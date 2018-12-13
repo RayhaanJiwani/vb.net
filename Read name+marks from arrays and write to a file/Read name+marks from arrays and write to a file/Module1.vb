6c8b8cb9f03ac5bb8a710ace0f860090b7719bc5
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:44:58 +0530

    Add files via upload

---
+++ Read name+marks from arrays and write to a file/Read name+marks from arrays and write to a file/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Names(10) As Char
+        Dim write, write_name, write_marks As String
+        Dim Marks(10), write_marks As Integer
+        Dim FileWrite As IO.StreamWriter
+        FileWrite = New IO.StreamWriter("C:\Users\rayhaan.jiwani\Desktop\Names+Marks.txt", True)
+        For counter = 1 To 10
+            write_name = Console.ReadLine()
+            Names(counter) = write_name
+            write_marks = Console.ReadLine()
+            Marks(counter) = write_marks
+            write = write_name + write_marks
+        Next
+    End Sub
+
+End Module
