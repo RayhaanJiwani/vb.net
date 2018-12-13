7572a957d615ff358c081f3371b5ddc47dc891e2
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:45:43 +0530

    Add files via upload

---
+++ Append contents of two files into a new one/Append contents of two files into a new one/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FileRead1, FileRead2 As IO.StreamReader
+        Dim FileWrite As IO.StreamWriter
+        Dim str As String
+        FileWrite = New IO.StreamWriter("C:\Users\rayhaan.jiwani\Desktop\Append_File.txt", True)
+        FileRead1 = New IO.StreamReader("C:\Users\rayhaan.jiwani\Desktop\Text.txt")
+        FileRead2 = New IO.StreamReader("C:\Users\rayhaan.jiwani\Desktop\Sample2.txt")
+
+        While FileRead1.EndOfStream = False
+            str = FileRead1.ReadLine
+            FileWrite.WriteLine(str)
+        End While
+        Console.WriteLine()
+        While FileRead2.EndOfStream = False
+            str = FileRead2.ReadLine
+            FileWrite.WriteLine(str)
+        End While
+
+        FileRead1.Close()
+        FileRead2.Close()
+        FileWrite.Close()
+        Console.WriteLine("The contents of the files have been appended")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Append contents of two files into a new one/Append contents of two files into a new one/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Append contents of two files into a new one")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Append contents of two files into a new one")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("44a07a40-2b95-4f79-b5e0-e47d11c72fd7")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
