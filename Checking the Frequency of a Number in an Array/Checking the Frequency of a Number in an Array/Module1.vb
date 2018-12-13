2c09caca98932bfcbe57f641146758d4ce084e45
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:46:36 +0530

    Add files via upload

---
+++ Checking the Frequency of a Number in an Array/Checking the Frequency of a Number in an Array/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Assuming that 'array_length' is known and ArrayNum() has values in it'
+
+        Dim compare, Array_Num(30), frequency, array_length As Integer
+        frequency = 0
+        Console.WriteLine("Please enter the number you are looking for")
+        compare = Console.ReadLine()
+        'array_length = 10'
+        'For Counter = 1 To 10'
+        'Array_Num(Counter) = Console.ReadLine()'
+        'Next'
+
+        For counter = 1 To array_length
+            If compare = Array_Num(counter) Then
+                frequency = frequency + 1
+            End If
+        Next
+
+        Console.WriteLine("The number appeared " & frequency & " time(s) in the array")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
