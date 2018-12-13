e487dc4336ebc1cebcdcfffe7c2b3b5c57de57a0
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:31:38 +0530

    Add files via upload

---
+++ Factorial of a Number/Factorial of a Number/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num, factorial As Integer
+        Console.WriteLine("Please enter the number you want a factorial of")
+        num = Console.ReadLine()
+        factorial = 1
+
+        For counter = 0 To (num - 1)
+            factorial = factorial + (factorial * counter)
+        Next
+
+        Console.WriteLine("The factorial of the number is " & factorial)
+        Console.ReadLine()
+    End Sub
+
+End Module
