7b8047008cb65a3ac27f5e39f2f2ab02a683c2e9
RayhaanJiwani <45856877+RayhaanJiwani@users.noreply.github.com>
2018-12-13 23:42:20 +0530

    Add files via upload

---
+++ Read names+marks from a file and store in arrays/Read names+marks from a file and store in arrays/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FileRead As IO.StreamReader
+        Dim temp, concat As String
+        Dim counter2, counter3, i, element As Integer
+        Dim marks(3), names(3) As String
+        concat = ""
+        element = 0
+        counter3 = 1
+        FileRead = New IO.StreamReader("C:\Users\rayhaan.jiwani\Desktop\Names+Marks.txt")
+        While FileRead.EndOfStream = False
+            element = element + 1
+            temp = FileRead.ReadLine
+            i = Len(temp)
+
+            Do Until Mid(temp, counter3, 1) = " "
+                concat = concat + Mid(temp, counter3, 1)
+                counter3 = counter3 + 1
+            Loop
+            names(element) = concat
+            concat = ""
+
+            For counter2 = counter3 To i
+                concat = concat + Mid(temp, counter2, 1)
+            Next
+            marks(element) = concat
+            concat = ""
+
+            counter3 = 1
+            Console.Write(names(element))
+            Console.Write(marks(element))
+            Console.WriteLine()
+        End While
+        FileRead.Close()
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Read names+marks from a file and store in arrays/Read names+marks from a file and store in arrays/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Read names+marks from a file and store in arrays")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Read names+marks from a file and store in arrays")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("fdb76e6c-6b8c-433e-9935-1a7211334b39")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
